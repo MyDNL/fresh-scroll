8c73f074d9c7c0b6360f573c137d3d393f44c508
MyDNL <100467725+MyDNL@users.noreply.github.com>
2024-04-08 17:29:44 +0300

    Update nav.tsx

--- islands/nav.tsx
+++ islands/nav.tsx
@@ -5,6 +5,7 @@ export function Nav() {
     <div class="bg-slate-50 mx-auto max-w-screen-xl px-4 py-2 sm:px-6 lg:px-8 shadow-md sticky top-0 z-50">
       <div class="flex justify-end">
         <a
+          target="_blank"
           href="https://github.com/MyDNL/fresh-scroll"
           class="text-gray-700 transition hover:opacity-75"
         >
